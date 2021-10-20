2bfdf70095a7976c8150b9719ceb0a482a09598b
bung87 <crc32@qq.com>
2021-10-20 14:58:37 +0800

    generate for ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import fg from "fast-glob";
 import { promisify } from "util";
 import { readFile, writeFile } from "fs";
 import path from "path";
+import pathExists = require('path-exists');
 
 const _readFile = promisify(readFile);
 const _writeFile = promisify(writeFile);
@@ -50,6 +51,20 @@ module.exports = {
   )}.map(p => path.join(__dirname,p))
 }
 `;
+    return c;
+  }
+
+export function generateIndexStringTs(options: NestOptions, middleContent: string): string {
+const c = `
+import * as path from 'path";
+${middleContent}
+export const nestOptions = {
+  package: ${JSON.stringify(options.package)},
+  protoPath:${JSON.stringify(
+    options.protoPath
+  )}.map(p => path.join(__dirname,p))
+}
+`;
   return c;
 }
 
@@ -58,7 +73,17 @@ export async function generateIndexFile(
   dest: string,
   root?: string
 ): Promise<void> {
-  const o = await generateIndex(source, root);
-  const content = generateIndexString(o);
-  await _writeFile(path.join(dest, "index.js"), content);
+  const options = await generateIndex(source, root);
+  const destIndexTs = path.join(dest, "index.ts");
+  const ts = await pathExists(destIndexTs);
+  const destIndexPath = ts ? destIndexTs : path.join(dest, "index.js")
+  if( ts ) {
+    const original = await _readFile(destIndexPath,'utf8');
+    const content = generateIndexStringTs(options,original);
+    await _writeFile(destIndexPath, content);
+  } else {
+    const content = generateIndexString(options)
+    await _writeFile(destIndexPath, content);
+  }
+
 }
